aec40b666425af68503f86d585efe17d9930563d
Allan Furlani Nishigouri <allanfurlani@gmail.com>
2024-10-10 11:02:22 -0300

    chore: change eslint and prettier to biome

--- src/koin-root-config.ts
+++ src/koin-root-config.ts
@@ -1,29 +1,28 @@
 import {
-  registerApplication, start, LifeCycles,
-  getMountedApps
-} from "single-spa";
+  type LifeCycles,
+  getMountedApps,
+  registerApplication,
+  start,
+} from 'single-spa'
 
-const aplications = [
-  { 
-    name: '@koin/app-login', 
-    source: '@koin/app-login', 
-    route: ({ pathname }) => pathname === '/login' 
+const applications = [
+  {
+    name: '@koin/app-login',
+    source: '@koin/app-login',
+    route: ({ pathname }) => pathname === '/login',
   },
-  { 
-    name: '@koin/app1', 
-    source: '@koin/app1', 
-    route: ({ pathname }) => pathname === '/app1' 
-  },
-];
+]
 
-aplications.forEach((app) => registerApplication({
-  name: app.name,
-  app: () => System.import<LifeCycles>(app.source),
-  activeWhen: app.route
-}));
+for (const app of applications) {
+  registerApplication({
+    name: app.name,
+    app: () => System.import<LifeCycles>(app.source),
+    activeWhen: app.route,
+  })
+}
 
 start({
   urlRerouteOnly: true,
-});
+})
 
-console.log("Apps loaded: ", getMountedApps())
+console.log('Apps loaded: ', getMountedApps())
